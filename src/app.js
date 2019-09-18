4e3fdeb0000c26ddc3e4a1aa327108e22f108798
BillClinton <bclinton@appunto.net>
2019-09-18 00:22:14 -0400

    Set up routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,74 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-console.log(path.join(__dirname, '../public'));
+// Define paths for Express config
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// Set up handlebars engine and views location
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.get('/weather', (req, res) => {
+// Set up static directory
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    location: 'Philadelphia'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About the app',
+    location: 'Philadelphia'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    msg: 'This is a helpful help message'
+  });
+});
+
+app.get('/weatherold', (req, res) => {
   res.send({
     forecast: 'sunny',
     location: 'Philadelphia'
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    });
+  }
+  res.send({
+    forecast: 'sunny',
+    location: 'Philadelphia',
+    address: req.query.address
+  });
+});
+
+app.get('/help*', (req, res) => {
+  res.render('404', {
+    title: '404 Not found.',
+    msg: 'The requested help article was not found.'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 Not found.',
+    msg: 'Check your address.'
   });
 });
 
