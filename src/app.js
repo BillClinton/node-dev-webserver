02b246f705028f45811ef877afb290c30f2441a4
BillClinton <bclinton@appunto.net>
2019-09-18 02:32:06 -0400

    Add dynamic port for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('../utils/geocode');
 const forecast = require('../utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, '../public');
@@ -84,6 +85,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Webserver started.');
+app.listen(port, () => {
+  console.log('Webserver started on port .' + port);
 });
