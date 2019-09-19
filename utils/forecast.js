0f2c53fdd18b6a4268b7130b3b9f4e0516eddf98
BillClinton <bclinton@appunto.net>
2019-09-18 21:50:27 -0400

    Correct lat/long order on skynet query

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const forecast = ({ long, lat }, callback) => {
   const apiKey = process.env.weatherApiKey;
-  const url = `https://api.darksky.net/forecast/${apiKey}/${long},${lat}`;
+  const url = `https://api.darksky.net/forecast/${apiKey}/${lat},${long}`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -12,7 +12,7 @@ const forecast = ({ long, lat }, callback) => {
     } else {
       const c = body.currently;
       const d = body.daily;
-      const msg = `${d.data[0].summary}  It is currently ${c.temperature} degrees. There is a ${c.precipProbability}% chance of rain.`;
+      const msg = `${d.data[0].summary}  It is currently ${c.temperature} degrees. There is a ${c.precipProbability}% chance of rain. Tomorrow's forecast: ${d.data[1].summary}`;
       callback(undefined, { msg });
     }
   });
